58ea8346ed3c7c5ea561c163adf2abf839479b04
ravisankarravi <77284840+ravisankarravi@users.noreply.github.com>
2022-01-28 09:47:47 +0530

    Add files via upload

---
+++ Perimeter.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Perimeter
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length and breadth of Rectangle in cm: ");
+		int l = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Enter the radius of circle in cm : ");
+		int r = sc.nextInt();
+		Perimeter ab = new Perimeter(l,b);
+		ab = new Perimeter(r);
+	}
+	Perimeter(int r)
+	{
+		System.out.println("Perimeter of the circle = "+ (float)(2*3.14*r)+" cm");
+	}
+	Perimeter(int l, int b)
+	{
+		System.out.println("Perimeter of the Rectangle = "+ (2*(l+b))+" cm");
+	}
+
+	
+	
+	
+}
