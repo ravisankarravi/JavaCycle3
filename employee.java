08e05c66e85617f3f15831aaa7a903699fdcb067
ravisankarravi <77284840+ravisankarravi@users.noreply.github.com>
2022-01-28 11:50:02 +0530

    Update employee.java

--- employee.java
+++ employee.java
@@ -32,3 +32,42 @@ public static void main(String[] args)
     }
 }
 }
+/*
+Output
+Enter the number of employee : 5
+Enter the employee number of Employee 1:1
+Enter the name of employee Abhijith
+Enter the Phone Number of employee 9495969696
+Enter the employee number of Employee 2:2
+Enter the name of employee Adarsh
+Enter the Phone Number of employee 9696499696
+Enter the employee number of Employee 3:3
+Enter the name of employee Althaf
+Enter the Phone Number of employee 9495867896
+Enter the employee number of Employee 4:4
+Enter the name of employee Amal
+Enter the Phone Number of employee 6969585874
+Enter the employee number of Employee 5:5
+Enter the name of employee Aromal
+Enter the Phone Number of employee 9495968687
+
+Employee ID : 1
+Employee name : Abhijith
+Employee Phone Number : 9495969696
+
+Employee ID : 2
+Employee name : Adarsh
+Employee Phone Number : 9696499696
+
+Employee ID : 3
+Employee name : Althaf
+Employee Phone Number : 9495867896
+
+Employee ID : 4
+Employee name : Amal
+Employee Phone Number : 6969585874
+
+Employee ID : 5
+Employee name : Aromal
+Employee Phone Number : 9495968687
+*/
